eab1573a01ba2ae41dde242c948359e3400008fc
ankonmukherjee00 <74072191+ankonmukherjee00@users.noreply.github.com>
2026-06-27 02:59:09 +0530

    fix;app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker on EC2 via Jenkins CI/CD!"
+    return "Hello from devops!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
